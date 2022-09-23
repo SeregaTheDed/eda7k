2581e64147b7ddbc0f447e1ffa7801fa419a57a4
SeregaTheDed <shtanko33333@mail.ru>
2022-09-23 09:51:04 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eda7k.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
