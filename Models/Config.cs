79c0823459a4b968d32b4fc2b75a117a7d0e50f4
SeregaTheDed <shtanko33333@mail.ru>
2022-10-07 13:28:05 +0300

    add config model

---
+++ Models/Config.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eda7k.Models
+{
+    public class Config
+    {
+        public DateTime next_order_day { get; set; }
+        public DateTime last_time_to_do_order { get; set; }
+    }
+
+    public partial class ApplicationContext : DbContext
+    {
+        public DbSet<Config> Config { get; set; }
+    }
+}
