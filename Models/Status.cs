25bc43ae3967b6082a90fc6a2279693cf0c19f70
SeregaTheDed <shtanko33333@mail.ru>
2022-10-07 14:39:10 +0300

    add status model

---
+++ Models/Status.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eda7k.Models
+{
+    public class Status
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public partial class DBConnection : DbContext
+    {
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
