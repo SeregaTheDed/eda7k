a73846022e34c20cd18e2ff4db7406be7f39218a
SeregaTheDed <shtanko33333@mail.ru>
2022-10-08 19:18:16 +0300

    add product model

---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eda7k.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public int price { get; set; }
+        public int category_id { get; set; }
+        public int extra { get; set; }
+        public string name { get; set; }
+        public bool availability_tomorrow { get; set; }
+
+    }
+    public partial class DBConnection : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
