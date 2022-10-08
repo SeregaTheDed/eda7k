aaefca73bc5b11b23c985ea671ce36ff838675e2
SeregaTheDed <shtanko33333@mail.ru>
2022-10-08 19:18:07 +0300

    add order model

---
+++ Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eda7k.Models
+{
+    public class Order
+    {
+        public int id { get; set; }
+        public string customer_name { get; set; }
+        public DateTime date { get; set; }
+        public int user_id { get; set; }
+        public int status_id { get; set; }
+    }
+    public partial class DBConnection : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+    }
+}
