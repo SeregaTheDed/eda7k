9f21cd876c910b34f09dc6ef657bf183c6eafa07
SeregaTheDed <shtanko33333@mail.ru>
2022-10-08 18:38:13 +0300

    add category model

---
+++ Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eda7k.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+    public partial class DBConnection : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+    }
+}
