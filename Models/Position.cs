3e6a0931adaf64c5d35af1e7b27d85315c3b81e3
SeregaTheDed <shtanko33333@mail.ru>
2022-10-10 13:43:31 +0300

    add position model

---
+++ Models/Position.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eda7k.Models
+{
+    public class Position
+    {
+        public int id { get; set; }
+        public int status_id { get; set; }
+        public DateTime date { get; set; }
+        public int product_id_first { get; set; }
+        public int? product_id_second { get; set; }
+        public string customer_name { get; set; }
+        public int user_id { get; set; }
+    }
+    public partial class DBConnection : DbContext
+    {
+        public DbSet<Position> Positions { get; set; }
+
+    }
+}
