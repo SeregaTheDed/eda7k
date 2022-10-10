7841f0e9c3578d665f0f48a1bdd1f5fd8473083e
SeregaTheDed <shtanko33333@mail.ru>
2022-10-10 14:38:15 +0300

    add references table model

---
+++ Models/Rel_orders_products.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eda7k.Models
+{
+    [Keyless]
+    public class Rel_orders_product
+    {
+        public int order_id { get; set; }
+        public int product_id { get; set; }
+        public int count { get; set; }
+    }
+    public partial class DBConnection : DbContext
+    {
+        public DbSet<Rel_orders_product> Rel_orders_products { get; set; }
+    }
+}
