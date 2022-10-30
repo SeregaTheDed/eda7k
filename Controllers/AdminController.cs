7642d0543c63058e3ccfdf4162f34a50aa12e04b
SeregaTheDed <shtanko33333@mail.ru>
2022-10-30 17:40:43 +0300

    add get categories and small fix

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -18,6 +18,7 @@ namespace eda7k.Controllers
         {
             return View();
         }
+
         // GET: AdminController/auth
         public async Task<IActionResult> Auth()
         {
@@ -36,6 +37,7 @@ namespace eda7k.Controllers
             }
             return Ok();
         }
+
         [HttpPost]
         public async Task<IActionResult> UpdateProduct([FromBody]Product modifiedProduct)
         {
@@ -54,18 +56,16 @@ namespace eda7k.Controllers
             }
             return Ok();
         }
-        /*
- $.ajax({
-            method: "post",
-            url: "/admin/DeleteProductById",
-            data: {"id":10},
-            success: function (data) {
-                console.log(data);
-                },
-            error: function (er) {
-                }
-            });
-         */
+
+        // POST: AdminController/GetProducts
+        [HttpPost]
+        public async Task<IActionResult> GetCategories()
+        {
+            using (DBConnection db = new())
+            {
+                return new OkObjectResult(db.Categories.ToArray());
+            }
+        }
 
         // POST: AdminController/GetProducts
         [HttpPost]
@@ -76,6 +76,7 @@ namespace eda7k.Controllers
                 return new OkObjectResult(db.Products.ToArray());
             }
         }
+
         /// <summary>
         /// Принимает список новых продуктов и возвращает список всех продуктов(включая новые)
         /// </summary>
@@ -92,46 +93,6 @@ namespace eda7k.Controllers
             }
             
         }
-        /*
-arr = [
-    {
-        "price": 22,
-        "category_id": 2,
-        "extra": 2,
-        "name": "Картофельное пюре с маслом",
-        "availability_tomorrow": false
-    },
-         {
-        "price": 23,
-        "category_id": 2,
-        "extra": 2,
-        "name": "пюре1",
-        "availability_tomorrow": false
-    },
-         {
-        "price": 24,
-        "category_id": 2,
-        "extra": 2,
-        "name": "Картофельное пюре2 с маслом",
-        "availability_tomorrow": false
-    }
-]
-
-arr = JSON.stringify(arr);
-
- $.ajax({
-            dataType: 'json',
-            contentType: "application/json",
-            type: "POST",
-            url: "/admin/AddNewProducts",
-            data: arr,
-            success: function (data) {
-                console.log(data);
-                },
-            error: function (er) {
-                }
-            });
-         */
     }
 
 }
