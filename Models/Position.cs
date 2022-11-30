a4ce9ad3895dd87a3f3f219b1e7fe6b059c35ed1
SeregaTheDed <shtanko33333@mail.ru>
2022-11-30 19:52:19 +0300

    update position model

--- Models/Position.cs
+++ Models/Position.cs
@@ -5,11 +5,12 @@ namespace eda7k.Models
     public class Position
     {
         public int? id { get; set; }
-        public int status_id { get; set; } //need
+        public int status_id { get; set; } 
         public DateTime? date { get; set; }
-        public int product_id_first { get; set; } //need
-        public int? product_id_second { get; set; } //need if composite
-        public string customer_name { get; set; } //need
+        public int product_id_first { get; set; } 
+        public int? product_id_second { get; set; } 
+        public string? customer_name { get; set; } 
+        public bool with_sauce { get; set; } 
         public int? user_id { get; set; }
     }
     public partial class DBConnection : DbContext
