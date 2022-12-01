02d6ee30398410845fb60082926c6897c2e13fd6
SeregaTheDed <shtanko33333@mail.ru>
2022-12-01 20:42:41 +0300

    add comments

--- Models/Position.cs
+++ Models/Position.cs
@@ -4,14 +4,14 @@ namespace eda7k.Models
 {
     public class Position
     {
-        public int? id { get; set; }
+        public int? id { get; set; } 
         public int status_id { get; set; } 
-        public DateTime? date { get; set; }
+        public DateTime? date { get; set; } //not need in db
         public int product_id_first { get; set; } 
         public int? product_id_second { get; set; } 
-        public string? customer_name { get; set; } 
+        public string? customer_name { get; set; } //not need in db
         public bool with_sauce { get; set; } 
-        public int? user_id { get; set; }
+        public int? user_id { get; set; } //not need in db
         public int price { get; set; }
     }
     public partial class DBConnection : DbContext
