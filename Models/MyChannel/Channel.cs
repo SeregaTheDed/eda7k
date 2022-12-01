4fde04d09006f1077ee482d52bf942b01b29f22c
SeregaTheDed <shtanko33333@mail.ru>
2022-12-01 18:55:02 +0300

    Add MyChannel system

---
+++ Models/MyChannel/Channel.cs
@@ -0,0 +1,63 @@
+﻿namespace eda7k.Models.MyChannel
+{
+    public enum Operations
+    {
+        Add,
+        Delete
+    }
+    //Ольга Павловна, если вы это читаете - простите за этот код :)
+    //Это моя "альтернатива" лонг пул запросов. В интеренете такого не нашел, только похожее
+    //Надо рефакторить, но я не знаю как тут получше нарезать
+    public static class MyChannel
+    {
+        private class MyDispatcher : IDisposable
+        {
+            private SemaphoreSlim _semaphore;
+            public MyDispatcher(SemaphoreSlim semaphore)
+            {
+                _semaphore = semaphore; 
+            }
+
+            public void Dispose()
+            {
+                Console.WriteLine("Dispose!");
+                _semaphore.Release();
+            }
+        }
+
+        private static SemaphoreSlim _sync = new SemaphoreSlim(1);
+        private static SemaphoreSlim _semaphoreTaskAction = new SemaphoreSlim(0);
+        private static SemaphoreSlim _semaphoreReadWrite = new SemaphoreSlim(1);
+        private static Task<KeyValuePair<Operations, int>> _sheduleTask = SheduleTask();
+        private static KeyValuePair<Operations, int> _operationAndId;
+        public static async void Write(KeyValuePair<Operations, int> operationAndId)
+        {
+            await _semaphoreReadWrite.WaitAsync();
+            _operationAndId = operationAndId;
+            _semaphoreTaskAction.Release();
+            await _sync.WaitAsync();
+            await Task.Delay(100);//За это время все отложенные таски должны завершиться
+            _sync.Release();
+            await _semaphoreTaskAction.WaitAsync();
+            _semaphoreReadWrite.Release();
+        }
+        private static async Task<KeyValuePair<Operations, int>> SheduleTask()
+        {
+            await _semaphoreTaskAction.WaitAsync();
+            using (MyDispatcher myDispatcher = new MyDispatcher(_semaphoreTaskAction))
+            {
+                return _operationAndId;
+            }
+        }
+        public static async Task<KeyValuePair<Operations, int>> GetSheduleTask()
+        {
+            await _sync.WaitAsync();
+            if (_sheduleTask.IsCompleted)
+            {
+                _sheduleTask = SheduleTask();
+            }
+            _sync.Release();
+            return await _sheduleTask;
+        }
+    }
+}
